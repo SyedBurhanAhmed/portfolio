2c4e79b3a3bd83af95f581cf0dec72447f93fee7
Burhan <syedburhanahmedd@gmail.com>
2024-11-16 20:17:34 +0500

    Initial commit for my custom version

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,21 +1,23 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://SyedBurhanAhmed.github.io',
+  title: 'SBA.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Syed Burhan Ahmed',
+  role: 'AI Engineer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+  resume: '/resume.pdf',
   social: {
     linkedin: 'https://linkedin.com',
     github: 'https://github.com',
   },
 }
+
+
 
 const projects = [
   // projects can be added an removed
@@ -38,6 +40,14 @@ const projects = [
   },
   {
     name: 'Project 3',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 4',
     description:
       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
     stack: ['SASS', 'TypeScript', 'React'],
